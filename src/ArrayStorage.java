58ac858c0da7c8c1b59d7d1acb5bda7493fb3bea
Aleksander Yakovenko <yarl79@yandex.ru>
2023-04-21 20:27:15 +0300

    ArrayStorage class is completed!

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,60 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < getSize(); i++)
+            storage[i] = null;
     }
 
+
     void save(Resume r) {
+        storage[getSize()] = Objects.requireNonNull(r);
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume result = null;
+        for (int i = 0; i < getSize(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                result = storage[i];
+                break;
+            }
+        }
+        return result;
     }
 
     void delete(String uuid) {
+        if (uuid == null) return;
+
+        Resume deleted = get(uuid);
+        if (deleted == null) return;
+
+        int index = Arrays.asList(storage).indexOf(deleted);
+        if (index == -1) return;
+        int size = getSize();
+        for (int i = index; i < size - 1; i++)
+            storage[i] = storage[i + 1];
+
+        storage[size - 1] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, getSize());
     }
 
     int size() {
-        return 0;
+        return getSize();
+    }
+
+    private int getSize() {
+        int size = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                size = i;
+                break;
+            }
+        }
+        return size;
     }
 }
