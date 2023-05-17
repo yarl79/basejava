69b97c428d1168c6c51bc0e37017593c9ca69622
Aleksander Yakovenko <yarl79@yandex.ru>
2023-05-17 18:48:09 +0300

    update resume

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -1,9 +1,11 @@
 package ru.javawebinar.basejava.model;
 
+import java.util.Objects;
+
 /**
  * Initial resume class
  */
-public class Resume {
+public class Resume implements Comparable<Resume>{
 
     // Unique identifier
     private String uuid;
@@ -25,5 +27,23 @@ public class Resume {
 
     public void setUuid(String uuid) {
         this.uuid = uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
+
+    @Override
+    public int compareTo(Resume o) {
+        return uuid.compareTo(o.uuid);
     }
 }
